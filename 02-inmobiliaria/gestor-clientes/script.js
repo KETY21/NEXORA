6915ef015be10330cada41120a0dd03f1e95e378
KETY21 <73615041+KETY21@users.noreply.github.com>
2026-09-14 20:24:19 +0200

    Create script.js
    document.addEventListener("DOMContentLoaded", function () {
    
        const boton = document.querySelector(".boton");
    
        boton.addEventListener("click", function () {
            alert("Cliente guardado correctamente en NEXORA");
        });
    
    });

---
+++ 02-inmobiliaria/gestor-clientes/script.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", function () {
+
+    const boton = document.querySelector(".boton");
+
+    boton.addEventListener("click", function () {
+        alert("Cliente guardado correctamente en NEXORA");
+    });
+
+});
